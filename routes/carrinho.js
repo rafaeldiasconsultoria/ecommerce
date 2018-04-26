b15c43468f9a98d0fb07f6c565bc186d478493e9
Rafael Dias <FS027@dc.com.br>
2018-04-26 05:41:23 -0300

    inclusao do carrinho

--- routes/carrinho.js
+++ routes/carrinho.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var _produtos = [
+  {nome: 'Produto 1', valor: 123.45, desconto: 98.45, descricao: 'Produto Teste de teste', imagens: {principal: '\\images\\produtos\\2.jpg'} },
+  {nome: 'Produto 2', valor: 123.45, descricao: 'Produto Teste de teste', imagens: {principal: '\\images\\produtos\\2.jpg'}},
+  {nome: 'Produto 3', valor: 123.45, descricao: 'Produto Teste de teste', imagens: {principal: '\\images\\produtos\\2.jpg'}}
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('carrinho', {__produtos: _produtos});
 });
 
 module.exports = router;
