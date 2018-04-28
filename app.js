c925f39628bbc604e783f93dcc8b925fcc3b37d0
Rafael Dias <FS027@dc.com.br>
2018-04-28 02:24:22 -0300

    inclusao do carrinho

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var carrinhoRouter = require('./routes/carrinho');
+var checkoutRouter = require('./routes/checkout');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/carrinho', carrinhoRouter);
+app.use('/checkout', checkoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
