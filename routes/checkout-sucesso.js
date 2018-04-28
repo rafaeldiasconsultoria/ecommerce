bcb2185b5d04c5023a86de7c657a1b29e762e2e4
Rafael Dias <FS027@dc.com.br>
2018-04-28 16:39:30 -0300

    ajustes de layout e imagens

--- routes/checkout-sucesso.js
+++ routes/checkout-sucesso.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var _produtosCarrinho = [
-  {nome: 'Produto 1', valor: 123.45, desconto: 98.45, descricao: 'Produto Teste de teste', imagens: {principal: '\\images\\produtos\\2.jpg'} },
-  {nome: 'Produto 2', valor: 123.45, descricao: 'Produto Teste de teste', imagens: {principal: '\\images\\produtos\\2.jpg'}},
-  {nome: 'Produto 3', valor: 123.45, descricao: 'Produto Teste de teste', imagens: {principal: '\\images\\produtos\\2.jpg'}}
-];
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('checkout-sucesso', {__produtosCarrinho: _produtosCarrinho});
+  res.render('checkout-sucesso');
 });
 
 module.exports = router;
