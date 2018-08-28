23d156b6d060fb909a48ea74b7c6b18c840154c0
Rafael Dias <t_systems.rafael.dias@grupoboticario.com.br>
2018-08-28 15:35:39 -0300

    teste de ws

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Site
 var indexRouter = require('./routes/index');
 var carrinhoRouter = require('./routes/carrinho');
 var checkoutRouter = require('./routes/checkout');
 var checkoutSucessoRouter = require('./routes/checkout-sucesso');
 
+// API V1
+var apiCheckoutRouter = require('./routes/api/v1/checkout');
 var app = express();
 
 // view engine setup
@@ -21,10 +24,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Site
 app.use('/', indexRouter);
 app.use('/carrinho', carrinhoRouter);
 app.use('/checkout', checkoutRouter);
 app.use('/checkout-sucesso', checkoutSucessoRouter);
+
+// Api V1
+app.use('/api/v1/checkout', apiCheckoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
