c51b3aa5a50e32ca1803a5326854ab7a6528d054
Rafael Dias <Rafael.2dias@live.com>
2018-08-29 08:33:43 -0300

    Alteração no retorno do início da transação - teste

--- routes/api/v1/checkout.js
+++ routes/api/v1/checkout.js
@@ -5,17 +5,17 @@ var Request = require("request");
 
 /* GET users listing. */
 router.get('/iniciarTransacao', function (req, res, next) {
-    var data = '';
+    var data = '......';
 
     Request.post({
         "headers": { "content-type": "application/json" },
         "url": "https://ws.sandbox.pagseguro.uol.com.br/v2/sessions?" + process.env.PagSeguroAPIToken,
     }, (error, response, body) => {
         if (error) {
-            return console.dir(error);
+            //return console.dir(error);
         }
-        data = body;
-        console.dir(JSON.parse(body));
+        data = JSON.parse(body);
+        //console.dir(JSON.parse(body));
     });
 
     res.send(data);
