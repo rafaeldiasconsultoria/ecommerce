783ebb81277706717c35e45db238bd6db61f3129
Rafael Dias <FS027@dc.com.br>
2018-08-30 04:20:54 -0300

    Implementação do metodo de iniciar sessão

--- routes/api/v1/checkout.js
+++ routes/api/v1/checkout.js
@@ -2,18 +2,44 @@ var express = require('express');
 var router = express.Router();
 var Request = require('request');
 
-router.options('/iniciarTransacao', function (req, res, next) {
+// CORS
+// TODO: achar solução melhor
+router.options('/', function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
 });
 
-router.get('/iniciarTransacao', function (req, res, next) {
-    var data = '......';
+router.get('/', function (req, res, next) {
+    var data = '';
     var ulrServico = process.env.PagSeguroWS + 
                     "/sessions?email=" + process.env.PagSeguroEMail + 
                     "&token=" + process.env.PagSeguroAPIToken;
     Request.post({
         "headers": {"Content-Type": "application/x-www-form-urlencoded"},
         "url": ulrServico,
+    }, (error, response, body) => {
+        if (error) {
+            res.send(error);
+        }
+        
+        var parseString = require('xml2js').parseString;
+        parseString(response.body, function (err, result) {
+            data = result;
+        });
+
+        res.send(data);
+    });
+});
+
+router.post('/', function (req, res, next) {
+    console.log('req body: ' + req.body);
+    var data = '';
+    var ulrServico = process.env.PagSeguroWS + 
+                    "/transactions?email=" + process.env.PagSeguroEMail + 
+                    "&token=" + process.env.PagSeguroAPIToken;
+    Request.post({
+        "headers": {"Content-Type": "application/x-www-form-urlencoded"},
+        "url": ulrServico,
+        "body": req.body,
     }, (error, response, body) => {
         if (error) {
             res.send(error);
