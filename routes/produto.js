66fe326f200e35926a3c2a71595f6f7ee9ad6452
Rafael Dias <FS027@dc.com.br>
2018-09-08 14:21:38 -0300

    Incluindo produtos

--- routes/produto.js
+++ routes/produto.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var _produtos = [];
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+  _produtos = [{
+      "nome": "Produto de Teste de Teste",
+      "descricao": "Descrição do produto de teste de teste",
+      "preco": "R$ 45,56",
+      "precoPromocao": "R$ 34,56"
+    },
+    {
+      "nome": "Produto de Teste de Teste",
+      "descricao": "Descrição do produto de teste de teste",
+      "preco": "R$ 45,56",
+      "precoPromocao": "R$ 34,56"
+    },
+    {
+      "nome": "Produto de Teste de Teste",
+      "descricao": "Descrição do produto de teste de teste",
+      "preco": "R$ 45,56",
+      "precoPromocao": "R$ 34,56"
+    }];
+
+res.send(_produtos);
 });
 
 module.exports = router;
